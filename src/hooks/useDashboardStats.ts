2a1b941560947698237584acefe473adfd7a3a4e
tomaschwirkmann-collab <tomaschwirkmann@gmail.com>
2026-05-08 09:12:53 +0200

    feat: dashboard kpi hover tooltips

--- src/hooks/useDashboardStats.ts
+++ src/hooks/useDashboardStats.ts
@@ -7,6 +7,21 @@ export type PipelineBreakdownItem = {
   total_value: number;
 };
 
+export type HoverCompanyValue = {
+  company_name: string;
+  pipeline_name: string;
+  total_value: number;
+  deal_count: number;
+};
+
+export type HoverCompanyExpected = {
+  company_name: string;
+  pipeline_name: string;
+  expected_value: number;
+  avg_probability: number;
+  deal_count: number;
+};
+
 export type DashboardStats = {
   pipeline_value: number;
   won_value: number;
@@ -21,6 +36,9 @@ export type DashboardStats = {
   medium_leads: number;
   cold_leads: number;
   pipeline_breakdown: PipelineBreakdownItem[];
+  hover_pipeline_companies?: HoverCompanyValue[];
+  hover_won_companies?: HoverCompanyValue[];
+  hover_probability_companies?: HoverCompanyExpected[];
 };
 
 export function useDashboardStats() {
